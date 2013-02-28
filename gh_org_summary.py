0658008edfe5b083f35df8df1061f2cc2fb1827a
Matthew Perry <mperry@ecotrust.org>
2013-02-27 18:05:07 -0800

    pagination tweaks

--- gh_org_summary.py
+++ gh_org_summary.py
@@ -67,14 +67,26 @@ def get_projects_overview(org, name_filter=None):
             sys.stderr.write("------ %s" % repo['name'])
             sys.stderr.write("\n")
 
-            r = requests.get(issues_url % repo['full_name'])
-            issues = r.json()
+            url = issues_url % repo['full_name']
+            res = requests.get(url)
+            issues = res.json()
+
+            page = 1
+            while "next" in res.headers['link']:
+                page += 1
+                paged_url = url + "&page=%d" % page
+                res = requests.get(paged_url)
+                issues.extend(res.json())
+
+            print len(issues)
             milestones = {}
             for issue in issues:
                 m = issue['milestone']
+                if "Server" in issue['title']:
+                    print "#####################################################################"
                 if m:
                     if m['title'] not in milestones.keys():
-                        milestones[m['title']] = {'due': m['due_on'], 'hours': {}}
+                        milestones[m['title']] = {'due': m['due_on'], 'hours': {}, 'tasks': {}}
 
                     assignee = issue['assignee']
                     try:
@@ -96,15 +108,19 @@ def get_projects_overview(org, name_filter=None):
                     hours = val * convert_to_hours[units]
 
                     if assignee_login in milestones[m['title']]['hours']:
+                        milestones[m['title']]['tasks'][assignee_login] += 1
                         milestones[m['title']]['hours'][assignee_login] += hours
                     else:
+                        milestones[m['title']]['tasks'][assignee_login] = 1
                         milestones[m['title']]['hours'][assignee_login] = hours
+                else:
+                    print "NO MILESTONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
             project['milestones'] = milestones
             projects.append(project)
     return projects
 
 if __name__ == '__main__':
     org = "Ecotrust"
-    name_filter = ['land_owner_tools', 'madrona-priorities', 'nroc_boater_survey']
+    name_filter = ['land_owner_tools', ] #'madrona-priorities', 'growth-yield-batch']
     projects = get_projects_overview(org, name_filter)
     print json.dumps(projects, indent=2)
